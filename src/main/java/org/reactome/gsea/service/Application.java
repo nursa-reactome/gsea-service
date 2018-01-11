816e3770dd4d4a6134027a52096dcd49313e929e
FredLoney <loneyf@ohsu.edu>
2018-01-11 11:47:24 -0800

    Perform component scan.

--- src/main/java/org/reactome/gsea/service/Application.java
+++ src/main/java/org/reactome/gsea/service/Application.java
@@ -2,8 +2,10 @@ package org.reactome.gsea.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.reactome.gsea.service")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
